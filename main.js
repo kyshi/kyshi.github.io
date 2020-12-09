ee70a87f70249b2f8c511e03684294e6d586356a
kyshi <72043589+kyshi@users.noreply.github.com>
2020-12-09 20:12:13 +0300

    Open Grap Meta Tags and Theme Update

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
-var is_changed = 0;
+var is_changed = 1;
 function change_theme(){
-    is_changed += 1
-
     if(is_changed == 0){
         var root = document.querySelector(":root"); 
         root.style.setProperty("--main-color", "#222831");
         root.style.setProperty("--text-color", "#FFF");
-    }else if(is_changed == 1){
-        var root = document.querySelector(":root");
-        root.style.setProperty("--main-color", "#1B262C");
-        root.style.setProperty("--text-color", "#FFF");
+        document.getElementById("discord").src = "media/discord_white.png";
+        document.getElementById("github").src = "media/github_white.png";
+        document.getElementById("email").src = "media/email_white.png";
+        is_changed = 1
     }
-    else{
+    else if(is_changed == 1){
         var root = document.querySelector(":root");
-        root.style.setProperty("--main-color", "#002C40");
-        root.style.setProperty("--text-color", "#FFF");
-    }
-
-    if(is_changed >= 2){
-        is_changed = -1
+        root.style.setProperty("--main-color", "##FFF");
+        root.style.setProperty("--text-color", "#000");
+        document.getElementById("discord").src = "media/discord_black.png";
+        document.getElementById("github").src = "media/github_black.png";
+        document.getElementById("email").src = "media/email_black.png";
+        is_changed = 0
     }
 }
