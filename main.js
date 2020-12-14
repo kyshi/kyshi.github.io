04ced84aa93d8b7e356a0ee0b17f18a042472291
kyshi <72043589+kyshi@users.noreply.github.com>
2020-12-14 07:02:59 +0300

    Added remember theme and updated og meta

--- main.js
+++ main.js
@@ -1,21 +1,36 @@
-var is_changed = 1;
+var is_changed = 0;
+if(!localStorage.getItem("theme")) {
+    localStorage.setItem("theme", 0);
+}
+else{
+    is_changed = localStorage.getItem("theme");
+    change_theme();
+}
 function change_theme(){
     if(is_changed == 0){
         var root = document.querySelector(":root"); 
         root.style.setProperty("--main-color", "#222831");
         root.style.setProperty("--text-color", "#FFF");
-        document.getElementById("discord").src = "media/discord_white.png";
-        document.getElementById("github").src = "media/github_white.png";
-        document.getElementById("email").src = "media/email_white.png";
+        setTimeout(function(){ 
+            document.getElementById("discord").src = "media/discord_white.png";
+            document.getElementById("github").src = "media/github_white.png";
+            document.getElementById("email").src = "media/email_white.png";
+        }, 1); 
         is_changed = 1
+        localStorage.setItem("theme", 0);
+        console.log(is_changed);
     }
     else if(is_changed == 1){
         var root = document.querySelector(":root");
         root.style.setProperty("--main-color", "##FFF");
         root.style.setProperty("--text-color", "#000");
-        document.getElementById("discord").src = "media/discord_black.png";
-        document.getElementById("github").src = "media/github_black.png";
-        document.getElementById("email").src = "media/email_black.png";
+        setTimeout(function(){ 
+            document.getElementById("discord").src = "media/discord_black.png";
+            document.getElementById("github").src = "media/github_black.png";
+            document.getElementById("email").src = "media/email_black.png";
+        }, 1); 
         is_changed = 0
+        localStorage.setItem("theme", 1);
+        console.log(is_changed);
     }
 }
